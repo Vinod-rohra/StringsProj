752a613676e87cbb4b9104b79a4f03bfc069cf79
vinod-rohra <VRohra@VRohra>
2020-12-10 23:53:29 -0500

    strings new project

---
+++ src/StringsProgram.java
@@ -0,0 +1,19 @@
+
+public class StringsProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Madlibs!");
+		
+		System.out.println("My ___ sister accidentally ___ed all over her new ___");
+		
+		String adjective = "flamboyant";
+		String verb = "fart";
+		String noun = "pogo stick";
+		
+		System.out.println("New My " + adjective + " sister accidentally " + verb + " ed all over her new " + noun);
+		
+		
+	}
+
+}
